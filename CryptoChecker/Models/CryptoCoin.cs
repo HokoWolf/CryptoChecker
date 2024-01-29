d2b71ebc35f2b7ff6c750300297e1616cc3ac2a3
Artem Tkach <artem.tkach004@gmail.com>
2024-01-29 22:15:49 +0200

    Created ListView with Top50 Crypto Currencies on MainWindow

--- CryptoChecker/Models/CryptoCoin.cs
+++ CryptoChecker/Models/CryptoCoin.cs
@@ -10,5 +10,8 @@
         public long Market_Cap { get; set; }
         public int Market_Cap_Rank { get; set; }
         public long Total_Volume { get; set; }
-    }
+		public double Price_Change_Percentage_1h_In_Currency { get; set; }
+        public double Price_Change_Percentage_24h_In_Currency { get; set; }
+        public double Price_Change_Percentage_7d_In_Currency { get; set; }
+	}
 }
--- CryptoChecker/ViewModels/MainWindowViewModel.cs
+++ CryptoChecker/ViewModels/MainWindowViewModel.cs
@@ -25,12 +25,26 @@ namespace CryptoChecker.ViewModels
 			{
 				client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36");
 
-				var response = await client.GetAsync("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10&page=1&precision=full");
+				var response = await client.GetAsync("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=50&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d");
 
 				if (response.IsSuccessStatusCode)
 				{
 					var result = await response.Content.ReadAsStringAsync();
 					CoinsList = new ObservableCollection<CryptoCoin>(JsonConvert.DeserializeObject<List<CryptoCoin>>(result)!);
+
+					foreach (var coin in CoinsList)
+					{
+						coin.Symbol = coin.Symbol.ToUpper();
+
+						coin.Price_Change_Percentage_1h_In_Currency = 
+							Math.Round(coin.Price_Change_Percentage_1h_In_Currency, 1);
+
+						coin.Price_Change_Percentage_24h_In_Currency =
+							Math.Round(coin.Price_Change_Percentage_24h_In_Currency, 1);
+
+						coin.Price_Change_Percentage_7d_In_Currency =
+							Math.Round(coin.Price_Change_Percentage_7d_In_Currency, 1);
+					}
 				}
 			}
 		}
